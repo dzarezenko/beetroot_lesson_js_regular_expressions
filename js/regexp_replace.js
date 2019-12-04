e91da0982023d633c54b055ea4e522b92c59932c
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-04 20:46:56 +0200

    ucfirst :)

--- js/regexp_replace.js
+++ js/regexp_replace.js
@@ -1,3 +1,7 @@
+let ucfirst = (s) => {
+  return s[0].toUpperCase() + s.slice(1);
+}
+
 // Literal way
 let regExpLit = /(ab+c)/gi; // "abc"
 console.log(regExpLit);
@@ -12,4 +16,7 @@ result = str.replace(regExpLit, "-$1-");
 console.log(result);
 
 result = str.replace(regExpLit, e => e.toUpperCase());
+console.log(result);
+
+result = str.replace(regExpLit, ucfirst);
 console.log(result);
